5259774546b32197f6013a698f63d842678aff6a
FARHAN SHAMIM <farzshamim25@gmail.com>
2023-05-20 17:45:52 +0530

    commitedsfsddddds

--- strings.py
+++ strings.py
@@ -1,14 +1,4 @@
 story = "hello , how are u doing? its been a long time , lets catch up someday"
-print(story.endswith("someday"))
-print(story.capitalize())
-print(story.count("a"))
-print(type("story"))
-<<<<<<< HEAD
-print ( i have replaced it )
-print(story.count("a")) new(name[, data])
-=======
-print(story.replace("hello", "hi"))
-jsdjkdkgdhjsbdkhbsd
-djsgdigskhjbdkhjsbd
-nsgdigksdkj
->>>>>>> issue1
+print(story.dfdwfgrgffgendswith("someday"))
+print(story.ize())
+print(story
